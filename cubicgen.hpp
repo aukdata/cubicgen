958a6780fde0a70bca999661d2542b8dd6c25391
takuma <takuma.y0524@gmail.com>
2023-12-10 01:33:26 +0900

    add doc

--- cubicgen.hpp
+++ cubicgen.hpp
@@ -10,37 +10,72 @@
 #include <random>
 #include <bitset>
 
+/**
+ * @brief Represents a 3D vector with x, y, and z coordinates.
+ */
 struct vec3 {
-    double x, y, z;
+    double x, y, z;  // x, y, z coordinates
 };
 
-// cubic is arbitary size of LED cube
-// depth is 4-bit constant
+/**
+ * @brief Represents LED cube
+ * 
+ * @tparam N The size of the cube.
+ */
 template<size_t N>
 class cubic {
 private:
-    std::array<std::array<std::array<uint8_t, N>, N>, N> voxels;
-    int32_t duration;
+    std::array<std::array<std::array<uint8_t, N>, N>, N> voxels; // 3D array representing the voxels
+    int32_t duration; // duration in milliseconds
 
 public:
     cubic() = default;
 
+    /**
+     * Set the value of a voxel at the specified coordinates.
+     * 
+     * @param x The x-coordinate of the voxel.
+     * @param y The y-coordinate of the voxel.
+     * @param z The z-coordinate of the voxel.
+     * @param value The value to set for the voxel.
+     */
     void set(int32_t x, int32_t y, int32_t z, uint8_t value) {
         voxels[x][y][z] = value;
     }
 
+    /**
+     * Set the duration of the cubic object.
+     * 
+     * @param _duration The duration in milliseconds.
+     */
     void set_duration(int32_t _duration) {
         duration = _duration;
     }
 
+    /**
+     * Get the value of a voxel at the specified coordinates.
+     * 
+     * @param x The x-coordinate of the voxel.
+     * @param y The y-coordinate of the voxel.
+     * @param z The z-coordinate of the voxel.
+     * @return The value of the voxel.
+     */
     int8_t get(int32_t x, int32_t y, int32_t z) const {
         return voxels[x][y][z];
     }
 
+    /**
+     * Get the duration of the cubic object.
+     * 
+     * @return The duration in milliseconds.
+     */
     int32_t get_duration() const {
         return duration;
     }
 
+    /**
+     * Print the cubic object.
+     */
     void print() const {
         std::cout << "cubic print" << std::endl;
         std::cout << "duration: " << duration << " ms" << std::endl;
@@ -56,25 +91,32 @@ public:
     }
 };
 
+/**
+ * @brief Represents a sequence of cubic objects.
+ * 
+ * @tparam N The size of the cube
+ */
 template<size_t N>
 class cubic_sequence {
 private:
-    std::vector<cubic<N>> cubics;
-    const std::array<uint8_t, 4> signature = { 0x43, 0x0d, 0x0a, 0x00 };
+    std::vector<cubic<N>> cubics; /**< The vector of cubic objects. */
+    const std::array<uint8_t, 4> signature = { 0x43, 0x0d, 0x0a, 0x00 }; /**< The signature of the file format. */
 
 public:
+
+    /**
+     * @brief Appends a cubic object to the sequence.
+     * @param _cubic The cubic object to append.
+     */
     void append(const cubic<N>& _cubic) {
         cubics.push_back(_cubic);
     }
 
-    // Write to file
-    // File format:
-    // 4 bytes: signature (0x43, 0x0d, 0x0a, 0x00)
-    // 4 bytes: frame count
-    // ============= Frames ================
-    // N * ceil(N * N / 2) bytes: frame
-    // 4 bytes: duration
-    // ===================================== x frame count
+    /**
+     * @brief Writes the cubic sequence to a file.
+     * @param filepath The path of the file to write to.
+     * @return True if the write operation is successful, false otherwise.
+     */
     bool write(const std::string& filepath) const {
         constexpr size_t s = std::ceil(1. * N * N / 2);
         std::ofstream writer(filepath, std::ios::binary);
@@ -119,37 +161,112 @@ public:
     }
 };
 
+/**
+ * @brief The base class for all shapes in the cubicgen library.
+ * 
+ * This class provides common functionality and interface for all shapes.
+ */
 class shape_interface {
 protected:
-    bool erased = false;
-    int64_t id;
-    static std::random_device rd;
-    static std::mt19937 gen;
-    static std::uniform_int_distribution<int64_t> dis;
+    bool erased = false; /* Flag indicating if the shape is erased. */
+    int64_t id; /* The unique identifier of the shape. */
+    static std::random_device rd; /* Random device used for generating random numbers. */
+    static std::mt19937 gen; /* Random number generator engine. */
+    static std::uniform_int_distribution<int64_t> dis; /* Uniform distribution for generating random numbers. */
 
 public:
+    /**
+     * @brief Default constructor.
+     * 
+     * Initializes the shape with a unique identifier.
+     */
     shape_interface() {
         id = dis(gen);
     }
+
+    /**
+     * @brief Virtual destructor.
+     */
     virtual ~shape_interface() {}
 
+    /**
+     * @brief Get the unique identifier of the shape.
+     * 
+     * @return The unique identifier of the shape.
+     */
     virtual int64_t get_id() const {
         return id;
     }
 
+    /**
+     * @brief Check if the shape is erased.
+     * 
+     * @return True if the shape is erased, false otherwise.
+     */
     virtual bool is_erased() const {
         return erased;
     }
 
+    /**
+     * @brief Create a clone of the shape.
+     * 
+     * @return A unique pointer to the cloned shape.
+     */
     virtual std::unique_ptr<shape_interface> clone() const = 0;
+
+    /**
+     * @brief Scale the shape by a given scale factor.
+     * 
+     * @param scale_factor The scale factor to apply to the shape.
+     * @return A unique pointer to the scaled shape.
+     */
     virtual std::unique_ptr<shape_interface> scale(const vec3& scale_factor) = 0;
+
+    /**
+     * @brief Move the shape by a given move vector.
+     * 
+     * @param move_vector The move vector to apply to the shape.
+     * @return A unique pointer to the moved shape.
+     */
     virtual std::unique_ptr<shape_interface> move(const vec3& move_vector) = 0;
+
+    /**
+     * @brief Erase the shape.
+     * 
+     * @return A unique pointer to the erased shape.
+     */
     virtual std::unique_ptr<shape_interface> erase() = 0;
+
+    /**
+     * @brief Check if the shape includes a given point.
+     * 
+     * @param point The point to check.
+     * @return True if the shape includes the point, false otherwise.
+     */
     virtual bool includes(const vec3& point) const = 0;
+
+    /**
+     * @brief Perform linear interpolation between this shape and another shape.
+     * 
+     * @param other The other shape to interpolate with.
+     * @param t The interpolation parameter.
+     * @return A unique pointer to the interpolated shape.
+     */
     virtual std::unique_ptr<shape_interface> lerp(const shape_interface& other, double t) const = 0;
+
+    /**
+     * @brief Get a debug string representation of the shape.
+     * 
+     * @return The debug string representation of the shape.
+     */
     virtual std::string debug() const = 0;
 
 protected:
+    /**
+     * @brief Set the unique identifier of the shape.
+     * 
+     * @param _id The unique identifier to set.
+     */
     void set_id(int64_t _id) {
         id = _id;
     }
@@ -159,20 +276,49 @@ std::random_device shape_interface::rd;
 std::mt19937 shape_interface::gen{ shape_interface::rd() };
 std::uniform_int_distribution<int64_t> shape_interface::dis{};
 
+/**
+ * @brief Represents a cuboid shape.
+ * 
+ * The cuboid class inherits from the shape_interface class and provides
+ * functionality to create, manipulate, and query cuboids in 3D space.
+ */
 class cuboid : public shape_interface {
 private:
     vec3 position, size;
 
 public:
+    /**
+     * @brief Construct a new cuboid object.
+     * 
+     * @param position The position of the cuboid.
+     * @param size The size of the cuboid.
+     */
     cuboid(const vec3& position, const vec3& size) : position(position), size(size) {}
+
+    /**
+     * @brief Construct a new cuboid object
+     * 
+     * @param other 
+     */
     cuboid(const cuboid& other) : position(other.position), size(other.size) {}
 
+    /**
+     * @brief Create a clone of the cuboid.
+     * 
+     * @return A unique pointer to the cloned cuboid.
+     */
     std::unique_ptr<shape_interface> clone() const override {
         auto ptr = std::make_unique<cuboid>(*this);
         ptr->set_id(id);
         return ptr;
     }
 
+    /**
+     * @brief Scale the cuboid by a given scale factor.
+     * 
+     * @param scale_factor The scale factor to apply to the cuboid.
+     * @return A unique pointer to the scaled cuboid.
+     */
     std::unique_ptr<shape_interface> scale(const vec3& scale_factor) override {
         size.x *= scale_factor.x;
         size.y *= scale_factor.y;
@@ -181,6 +327,12 @@ public:
         return clone();
     }
 
+    /**
+     * @brief Move the cuboid by a given move vector.
+     * 
+     * @param move_vector The move vector to apply to the cuboid.
+     * @return A unique pointer to the moved cuboid.
+     */
     std::unique_ptr<shape_interface> move(const vec3& move_vector) override {
         position.x += move_vector.x;
         position.y += move_vector.y;
@@ -189,18 +341,36 @@ public:
         return clone();
     }
 
+    /**
+     * @brief Erase the cuboid.
+     * 
+     * @return A unique pointer to the erased cuboid.
+     */
     std::unique_ptr<shape_interface> erase() override {
         erased = true;
         std::cout << "erased " << std::to_string(id) << ", " << debug() << std::endl;
         return clone();
     }
 
+    /**
+     * @brief Check if the cuboid includes a given point.
+     * 
+     * @param point The point to check.
+     * @return True if the cuboid includes the point, false otherwise.
+     */
     bool includes(const vec3& point) const override {
         return (point.x >= position.x && point.x <= position.x + size.x &&
                 point.y >= position.y && point.y <= position.y + size.y &&
                 point.z >= position.z && point.z <= position.z + size.z);
     }
 
+    /**
+     * @brief Perform linear interpolation between this cuboid and another cuboid.
+     * 
+     * @param other The other cuboid to interpolate with.
+     * @param t The interpolation parameter.
+     * @return A unique pointer to the interpolated cuboid.
+     */
     std::unique_ptr<shape_interface> lerp(const shape_interface& other, double t) const override {
         // check if the other shape has the same id
         if (id != other.get_id()) {
@@ -220,27 +390,68 @@ public:
         return result;
     }
 
+    /**
+     * @brief Get a debug string representation of the cuboid.
+     * 
+     * @return The debug string representation of the cuboid.
+     */
     std::string debug() const override {
         return "cuboid, pos: (" + std::to_string(position.x) + ", " + std::to_string(position.y) + ", " + std::to_string(position.z) + "), size: (" + std::to_string(size.x) + ", " + std::to_string(size.y) + ", " + std::to_string(size.z) + ")";
     }
 };
 
+/**
+ * @brief Represents an ellipsoid shape.
+ * 
+ * The ellipsoid class inherits from the shape_interface class and provides
+ * functionality to create, manipulate, and query ellipsoids in 3D space.
+ */
 class ellipsoid : public shape_interface {
 private:
     vec3 position;
     vec3 radii;
 
 public:
+    /**
+     * @brief Construct a new ellipsoid object.
+     * 
+     * @param position The position of the ellipsoid.
+     * @param radii The radii of the ellipsoid.
+     */
     ellipsoid(const vec3& position, const vec3& radii) : position(position), radii(radii) {}
+
+    /**
+     * @brief Construct a new ellipsoid object.
+     * 
+     * @param position The position of the ellipsoid.
+     * @param radii The radii of the ellipsoid.
+     */
     ellipsoid(const vec3& position, double radii) : position(position), radii({radii, radii, radii}) {}
+
+    /**
+     * @brief Construct a new ellipsoid object.
+     * 
+     * @param other 
+     */
     ellipsoid(const ellipsoid& other) : position(other.position), radii(other.radii) {}
 
+    /**
+     * @brief Create a clone of the ellipsoid.
+     * 
+     * @return A unique pointer to the cloned ellipsoid.
+     */
     std::unique_ptr<shape_interface> clone() const override {
         auto ptr = std::make_unique<ellipsoid>(*this);
         ptr->set_id(id);
         return ptr;
     }
 
+    /**
+     * @brief Scale the ellipsoid by a given scale factor.
+     * 
+     * @param scale_factor The scale factor to apply to the ellipsoid.
+     * @return A unique pointer to the scaled ellipsoid.
+     */
     std::unique_ptr<shape_interface> scale(const vec3& scale_factor) override {
         radii.x *= scale_factor.x;
         radii.y *= scale_factor.y;
@@ -249,6 +460,12 @@ public:
         return clone();
     }
 
+    /**
+     * @brief Move the ellipsoid by a given move vector.
+     * 
+     * @param move_vector The move vector to apply to the ellipsoid.
+     * @return A unique pointer to the moved ellipsoid.
+     */
     std::unique_ptr<shape_interface> move(const vec3& move_vector) override {
         position.x += move_vector.x;
         position.y += move_vector.y;
@@ -257,12 +474,23 @@ public:
         return clone();
     }
 
+    /**
+     * @brief Erase the ellipsoid.
+     * 
+     * @return A unique pointer to the erased ellipsoid.
+     */
     std::unique_ptr<shape_interface> erase() override {
         erased = true;
         std::cout << "erased " << std::to_string(id) << ", " << debug() << std::endl;
         return clone();
     }
 
+    /**
+     * @brief Check if the ellipsoid includes a given point.
+     * 
+     * @param point The point to check.
+     * @return True if the ellipsoid includes the point, false otherwise.
+     */
     bool includes(const vec3& point) const override {
         double dx = (point.x - position.x) / radii.x;
         double dy = (point.y - position.y) / radii.y;
@@ -270,6 +498,13 @@ public:
         return (dx * dx + dy * dy + dz * dz <= 1.0);
     }
 
+    /**
+     * @brief Perform linear interpolation between this ellipsoid and another ellipsoid.
+     * 
+     * @param other The other ellipsoid to interpolate with.
+     * @param t The interpolation parameter.
+     * @return A unique pointer to the interpolated ellipsoid.
+     */
     std::unique_ptr<shape_interface> lerp(const shape_interface& other, double t) const override {
         // check if the other shape has the same id
         if (id != other.get_id()) {
@@ -293,28 +528,58 @@ public:
         return result;
     }
 
+    /**
+     * @brief Get a debug string representation of the ellipsoid.
+     * 
+     * @return The debug string representation of the ellipsoid.
+     */
     std::string debug() const override {
         return "ellipsoid, pos: (" + std::to_string(position.x) + ", " + std::to_string(position.y) + ", " + std::to_string(position.z) + "), radii: (" + std::to_string(radii.x) + ", " + std::to_string(radii.y) + ", " + std::to_string(radii.z) + ")";
     }
 };
 
+/**
+ * @brief Represents a shape sequence.
+ * 
+ * The sphere class inherits from the shape_interface class and provides
+ * functionality to create, manipulate, and query spheres in 3D space.
+ */
 class shape_sequence {
 private:
     std::vector<std::pair<int32_t, std::unique_ptr<shape_interface>>> keyframes;
 
 public:
-    ~shape_sequence() = default;
-
+    /**
+     * @brief Add a keyframe to the shape sequence.
+     * 
+     * @param time The time of the keyframe in milliseconds.
+     * @param shape The shape to add to the keyframe.
+     */
     void add_keyframe(int32_t time, std::unique_ptr<shape_interface>&& shape) {
         std::cout << "keyframe added " << std::to_string(shape->get_id()) << ", " << shape->debug() << ", time: " << std::to_string(time) << std::endl;
         keyframes.push_back({time, std::move(shape)});
     }
 
+    /**
+     * @brief Add a keyframe to the shape sequence.
+     * 
+     * @param time The time of the keyframe in milliseconds.
+     * @param shape The shape to add to the keyframe.
+     */
     void add_keyframe(int32_t time, const shape_interface& shape) {
         std::cout << "keyframe added " << std::to_string(shape.get_id()) << ", " << shape.debug() << ", time: " << std::to_string(time) << std::endl;
         keyframes.push_back({ time, shape.clone() });
     }
 
+    /**
+     * @brief Rasterize the shape sequence into a cubic sequence.
+     * 
+     * @tparam N The size of the cubic sequence.
+     * @param depth The depth of the cubic sequence.
+     * @param resolution The resolution of the cubic sequence.
+     * @param until The time until which the sequence is rasterized.
+     * @return The rasterized cubic sequence.
+     */
     template<size_t N>
     cubic_sequence<N> rasterize(int32_t depth, int32_t resolution, int32_t until) {
         // This function rasterizes the animation into a cubic.
@@ -417,6 +682,14 @@ public:
         return result;
     }
 
+    /**
+     * @brief Rasterize the shape sequence into a cubic.
+     * 
+     * @tparam N The size of the cubic.
+     * @param shapes The shapes to rasterize.
+     * @param depth The depth of the cubic.
+     * @return The rasterized cubic.
+     */
     template<size_t N>
     cubic<N> rasterize_frame(const std::vector<std::unique_ptr<shape_interface>>& shapes, int32_t depth) {
         // This function rasterizes the animation into a cubic.
@@ -424,9 +697,9 @@ public:
 
         cubic<N> c{};
 
-        for (int32_t x = 0; x < N; ++x) {
-            for (int32_t y = 0; y < N; ++y) {
-                for (int32_t z = 0; z < N; ++z) {
+        for (size_t x = 0; x < N; ++x) {
+            for (size_t y = 0; y < N; ++y) {
+                for (size_t z = 0; z < N; ++z) {
                     const int32_t brightness = static_cast<int32_t>(calc_overlapping_percentage<N>(x, y, z, shapes, depth) * (std::pow(2, depth) - 1));
                     c.set(x, y, z, brightness);
                 }
@@ -436,15 +709,29 @@ public:
         return c;
     }
 
-    void print_keyframes() {
+    /**
+     * @brief Print the shape sequence.
+     */
+    void debug() {
         for (const auto& [time, shape] : keyframes) {
             std::cout << time << ": " << shape->get_id() << std::endl;
         }
     }
 
 private:
+    /**
+     * @brief Calculate the percentage of the shape that overlaps with each voxel.
+     * 
+     * @tparam N The size of the cubic.
+     * @param x The x-coordinate of the voxel.
+     * @param y The y-coordinate of the voxel.
+     * @param z The z-coordinate of the voxel.
+     * @param shapes The shapes to calculate the overlapping percentage with.
+     * @param depth The depth of the cubic.
+     * @return The percentage of the shape that overlaps with each voxel.
+     */
     template<size_t N>
-    double calc_overlapping_percentage(int32_t x, int32_t y, int32_t z, const std::vector<std::unique_ptr<shape_interface>>& shapes, int32_t depth) {
+    double calc_overlapping_percentage(size_t x, size_t y, size_t z, const std::vector<std::unique_ptr<shape_interface>>& shapes, int32_t depth) {
         // This function calculates the percentage of the shape that overlaps with each voxel.
         // The result is a 4-bit integer.
 
--- sample.cpp
+++ sample.cpp
@@ -3,27 +3,16 @@
 int main() {
     shape_sequence seq{};
 
-    
+    cuboid cub_obj{ vec3{ -0.3, -0.3, -0.3 }, vec3{ 0.4, 0.4, 0.4 } };
+    seq.add_keyframe(0, cub_obj);
+    seq.add_keyframe(1000, cub_obj.move({ 0.6, 0, 0 }));
+    seq.add_keyframe(2000, cub_obj.move({ 0, 0.6, 0 }));
+    seq.add_keyframe(3000, cub_obj.move({ 0, 0, 0.6 }));
+    seq.add_keyframe(4000, cub_obj.move({ -0.6, 0, 0 }));
+    seq.add_keyframe(5000, cub_obj.move({ 0, -0.6, 0 }));
+    seq.add_keyframe(6000, cub_obj.move({ 0, 0, -0.6 }));
 
-    /*
-    cuboid cube_obj{ vec3{ 0., 0., 0. }, vec3{ 1., 1., 1. } };
-
-    seq.add_keyframe(0, cube_obj);
-    seq.add_keyframe(1000, cube_obj.scale( vec3{ .5, .5, .5 } ));
-    seq.add_keyframe(2000, cube_obj.move( vec3{ 0.2, 0, 0 } ));
-    seq.add_keyframe(3000, cube_obj.erase());
-    */
-
-    ellipsoid ell_obj{ vec3{ 0., 0., 0. }, vec3{ .5, .5, .5 } };
-
-    seq.add_keyframe(0, ell_obj);
-    seq.add_keyframe(1000, ell_obj.scale({ 0.5, 0.5, 0.5 }));
-    seq.add_keyframe(1500, ell_obj.move({ 0.2, 0, 0 }));
-    seq.add_keyframe(2000, ell_obj.erase());
-
-    seq.print_keyframes();
-
-    const auto& c = seq.rasterize<5>(4, 12, 3000);  // 5x5x5 cube
+    const auto& c = seq.rasterize<5>(4, 12, 6000);  // suppose 5x5x5 cube
     c.write("test.cbi");
 
     return 0;
